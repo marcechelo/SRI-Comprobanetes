8cf0adc3cac37954a25bcfaecc5dedb7b3a7b59e
Marcelo Nieto <marcechelo25@hotmail.com>
2019-06-03 17:49:16 -0500

    Arreglo de datos como variable de session

--- Projet1/sri_test/urls.py
+++ Projet1/sri_test/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 from . import views
 from django.conf.urls import url
 from sri_test.views import sri_document
+from django.views.generic import RedirectView
+from django.urls import re_path
 
 app_name = 'sri_test'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', sri_document.as_view(), name='index'),
     path('comprobantes/', views.test, name= 'comprobantes'),
     path('downloadxml/', views.downloadxml, name= 'downloadxml'),
     path('downloadPdf/', views.downloadPdf, name= 'downloadPdf'),
